7235bbdd18cfddf054b7bbbc8fcf5ecd990ef4e5
Jeffrey Picard <jeff@jeffreypicard.com>
2019-10-17 22:23:55 -0400

    rename script again

--- scripts/init_and_run.sh
+++ scripts/init_and_run.sh
@@ -10,3 +10,8 @@ FLUSH PRIVILEGES;
 END
 mysql < /var/www/medieval-europe/sql/structure.sql
 mysql < /var/www/medieval-europe/sql/core_data.sql
+
+php5.6 /var/www/medieval-europe/public_html/database/scripts/Jdemolay/03.\ Reset_Server.php
+
+# This is run as a docker CMD so we just sleep forever
+while true; do sleep 1000; done
