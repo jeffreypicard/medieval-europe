24c7c06b67d8f643d17e9207958e614fdbd6d976
Jeffrey Picard <jeff@jeffreypicard.com>
2020-08-14 03:20:38 -0400

    asdf

--- public_html/application/models/st_court_1.php
+++ public_html/application/models/st_court_1.php
@@ -19,7 +19,7 @@ class ST_Court_1_Model extends Structure_Model
 	// Funzione che costruisce i links relativi
 	// @output: stringa contenente i links relativi a questa struttura
 	
-	public function build_common_links( $structure ) 
+	public function build_common_links( $structure, $bonus = false )
 	{
 		$links = parent::build_common_links( $structure );		
 
@@ -30,7 +30,7 @@ class ST_Court_1_Model extends Structure_Model
 		return $links;
 	}
 	
-	public function build_special_links( $structure )
+	public function build_special_links( $structure, $bonus = false )
 	{
 		
 		$links = parent::build_special_links( $structure );		
